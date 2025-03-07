48f367127210945067a24538c0829cb8983de7b5
Jacob <jcmuffin18@gmail.com>
2025-03-07 22:25:18 +0100

    changed to visualization page

--- GUI/app.py
+++ GUI/app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 home = st.Page("pages/home.py", title="Home", icon=":material/home:", default=True)
-explanation = st.Page("pages/explanation.py", title="Explanation", icon=":material/graphic_eq:")
+visualization = st.Page("pages/visualization.py", title="Visualization", icon=":material/graphic_eq:")
 chat = st.Page("pages/chat.py", title="Chat", icon=":material/sms:")
 
 pg = st.navigation(
     [
         home,
-        explanation,
+        visualization,
         chat
     ]
 )
