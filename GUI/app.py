d809fff3e5a0c605bdbcf5ec6629cfeb6c4b2cb1
Jacob <jcmuffin18@gmail.com>
2025-03-08 10:26:33 +0100

    restructured GUI

--- GUI/app.py
+++ GUI/app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 
 home = st.Page("pages/home.py", title="Home", icon=":material/home:", default=True)
-visualization = st.Page("pages/visualization.py", title="Visualization", icon=":material/graphic_eq:")
-chat = st.Page("pages/chat.py", title="Chat", icon=":material/sms:")
+explanation = st.Page("pages/explanation.py", title="Explanation", icon=":material/sms:")
 
 pg = st.navigation(
     [
         home,
-        visualization,
-        chat
+        explanation
     ]
 )
 
